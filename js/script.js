43984479c23d6f41554f69e07f8c610264abf228
Khasanov <zkhasanovv@gmail.com>
2021-02-04 11:26:13 +0200

    Created form "Add new event" in popup

--- js/script.js
+++ js/script.js
@@ -44,12 +44,67 @@ const defaultSelect = () => {
         searchEnabled: false,
         itemSelectText: ""
     });
-
     choices.setChoices(
         [
             {value: 0, label: 'All members', selected: true},
             {value: 1, label: 'John'},
             {value: 2, label: 'Sam'}
+        ]
+    );
+
+    const daysChoice = document.querySelector('.js-days');
+    const choices1 = new Choices(daysChoice, {
+        searchEnabled: false,
+        itemSelectText: ""
+    });
+
+    choices1.setChoices(
+        [
+            {value: 0, label: 'Monday', selected: true},
+            {value: 1, label: 'Tuesday'},
+            {value: 2, label: 'Wednesday'},
+            {value: 3, label: 'Thursday'},
+            {value: 4, label: 'Friday'}
+        ]
+    );
+
+    const timesChoice = document.querySelector('.js-times');
+    const choices2 = new Choices(timesChoice, {
+        searchEnabled: false,
+        searchResultLimit: 5,
+        itemSelectText: ""
+    });
+
+    choices2.setChoices(
+        [
+            {value: 0, label: '10:00', selected: true},
+            {value: 1, label: '11:00'},
+            {value: 2, label: '12:00'},
+            {value: 3, label: '13:00'},
+            {value: 4, label: '14:00'},
+            {value: 5, label: '15:00'},
+            {value: 6, label: '16:00'},
+            {value: 7, label: '17:00'},
+            {value: 8, label: '18:00'}
+        ]
+    );
+
+    const participantsChoice = document.querySelector('.js-participants');
+    const choices3 = new Choices(participantsChoice, {
+        searchEnabled: false,
+        searchResultLimit: 5,
+        itemSelectText: "",
+        placeholderValue: "asd"
+    });
+
+    choices3.setChoices(
+        [
+            {value: 0, label: 'John'},
+            {value: 1, label: 'Sam'},
+            {value: 2, label: 'Ann'},
+            {value: 3, label: 'Tommy'},
+            {value: 4, label: 'Hanna'},
+            {value: 5, label: 'Kenny'}
         ]
     );
 }
